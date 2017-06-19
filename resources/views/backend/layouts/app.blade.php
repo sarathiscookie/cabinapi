328ed5dcf8424db46e310978eadaebf7124ca81c
sarath <sarath@cabin-holiday.com>
2017-06-19 14:49:59 +0530

    API: Bootstrap admin panel added

---
+++ resources/views/backend/layouts/app.blade.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('css/AdminLTE.min.css') }}">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="{{ asset('css/skins/skin-blue.min.css') }}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('plugins/iCheck/flat/blue.css') }}">
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="{{ asset('plugins/morris/morris.css') }}">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="{{ asset('plugins/datepicker/datepicker3.css') }}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>A</b>PI</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>Cabin</b>API</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <!-- Messages: style can be found in dropdown.less-->
+                <li class="dropdown messages-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-envelope-o"></i>
+                        <span class="label label-success">4</span>
+                    </a>
+                </li>
+                <!-- Notifications: style can be found in dropdown.less -->
+                <li class="dropdown notifications-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-bell-o"></i>
+                        <span class="label label-warning">10</span>
+                    </a>
+                </li>
+                <!-- Tasks: style can be found in dropdown.less -->
+                <li class="dropdown tasks-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-flag-o"></i>
+                        <span class="label label-danger">9</span>
+                    </a>
+                </li>
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <span class="hidden-xs">Alexander Pierce</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <p>
+                                Alexander Pierce - Web Developer
+                                <small>Member since Nov. 2012</small>
+                            </p>
+                        </li>
+                        <!-- Menu Body -->
+                        <li class="user-body">
+                            <div class="row">
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Followers</a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Sales</a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Friends</a>
+                                </div>
+                            </div>
+                            <!-- /.row -->
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <!-- Control Sidebar Toggle Button -->
+                <li>
+                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="active treeview">
+                <a href="#">
+                    <i class="fa fa-dashboard"></i> <span>Users</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Users List</a></li>
+                    <li><a href="index2.html"><i class="fa fa-circle-o"></i> Users List</a></li>
+                </ul>
+            </li>
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-table"></i> <span>Booking</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i> Booking List</a></li>
+                    <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Send Voucher</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    @yield('content')
+</div>
+<!-- /.content-wrapper -->
+
+    <!-- Scripts -->
+    {{--<script src="{{ asset('js/app.js') }}"></script>--}}
+    <!-- jQuery 2.2.3 -->
+    <script src="{{ asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button);
+    </script>
+    <!-- Bootstrap 3.3.6 -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <!-- Morris.js charts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="{{ asset('plugins/morris/morris.min.js') }}"></script>
+    <!-- Sparkline -->
+    <script src="{{ asset('plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+    <!-- jvectormap -->
+    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+    <script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="{{ asset('plugins/knob/jquery.knob.js') }}"></script>
+    <!-- daterangepicker -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+    <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <!-- datepicker -->
+    <script src="{{ asset('plugins/datepicker/bootstrap-datepicker.js') }}"></script>
+    <!-- Bootstrap WYSIHTML5 -->
+    <script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+    <!-- Slimscroll -->
+    <script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('plugins/fastclick/fastclick.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('js/app.min.js') }}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('js/demo.js') }}"></script>
+    <!-- Bootstrap WYSIHTML5 -->
+    <script src="{{ asset('plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,190 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*Route::get('/test', 'TestmongoController@index')->name('test');*/
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function () {
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here we define general routes
+    |
+    */
+
+    /* Listing countries */
+    Route::get('/countries', 'CountryController@index');
+
+    /* Listing clubs */
+    Route::get('/clubs', 'ClubController@index');
+
+    /* Listing lands */
+    Route::get('/lands', 'LandController@index');
+
+    /* Listing facilities */
+    Route::get('/facilities', 'FacilityController@index');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dashboard Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here we define dashboard routes
+    |
+    */
+
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes for users
+    |--------------------------------------------------------------------------
+    |
+    | Routes for listing, update, delete users
+    | Routes for add club members
+    | Routes for assign roles.
+    */
+    /* Listing users */
+    Route::get('/users', 'UserlistController@index'); //http://cabinapi.app/api/users?page=9
+
+    /* Get information from user */
+    Route::get('/users/{id}', 'UserlistController@show');
+
+    /* Update user status to activate and deactivate */
+    Route::put('/users/status/{statusId}/{id}', 'UserlistController@statusUpdate');
+
+    /* Update user status to activate and deactivate */
+    Route::put('/users/role/{roleId}/{id}', 'UserlistController@roleUpdate');
+
+    /* Update user details */
+    Route::put('/users/edit/{id}', 'UserlistController@update');
+
+    /* Add or Update club */
+    Route::put('/users/club/{id}', 'UserlistController@clubUpdate');
+
+    /* Delete user */
+    Route::delete('/users/{id}', 'UserlistController@destroy');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes for bookings
+    |--------------------------------------------------------------------------
+    |
+    | Routes for listing & delete bookings
+    | Routes for update payment
+    | Routes for send vouchers.
+    */
+    /* Listing bookings */
+    Route::get('/bookings', 'BookingController@index'); //http://cabinapi.app/api/bookings?page=9
+
+    /* Get individual information from bookings */
+    Route::get('/bookings/{id}', 'BookingController@show');
+
+    /* Update payment status */
+    Route::put('/bookings/payment/{status}/{id}', 'BookingController@update');
+
+    /* Send invoice*/
+    Route::post('/bookings/voucher/{id}', 'BookingController@sendInvoice');
+
+    /* Delete bookings */
+    Route::delete('/bookings/{id}', 'BookingController@destroy');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes for roles
+    |--------------------------------------------------------------------------
+    |
+    | Routes for list, edit, add & delete role
+    */
+    /* Listing roles */
+    Route::get('/roles', 'RoleController@index'); //http://cabinapi.app/api/roles?page=9
+
+    /* Create role */
+    Route::post('/roles', 'RoleController@store');
+
+    /* Update roles */
+    Route::put('/roles/{id}', 'RoleController@update');
+
+    /* Deleted roles */
+    Route::delete('/roles/{id}', 'RoleController@destroy');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes for regions
+    |--------------------------------------------------------------------------
+    |
+    | Routes for list, edit, add & delete regions
+    */
+    /* Listing regions */
+    Route::get('/regions', 'RegionController@index'); //http://cabinapi.app/api/regions?page=9
+
+    /* Create regions */
+    Route::post('/regions', 'RegionController@store');
+
+    /* Update regions */
+    Route::put('/regions/{id}', 'RegionController@update');
+
+    /* Deleted regions */
+    Route::delete('/regions/{id}', 'RegionController@destroy');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes for fetch booking within date range and send vouchers
+    |--------------------------------------------------------------------------
+    |
+    | Routes for completed booking within date range & send vouchers
+    */
+    /* Listing vouchers */
+    Route::get('/daterange/bookings/{beginDate?}/{endDate?}', 'InvoiceController@index'); //http://cabinapi.app/api/bookings/completed?page=9
+
+    /* Send bulk invoice */
+    Route::post('/daterange/bookings/invoices/send', 'InvoiceController@sendInvoice');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes for cabins
+    |--------------------------------------------------------------------------
+    |
+    | Routes for list, edit, add & delete cabins
+    | Routes for update cabin price
+    | Routes for update season date
+    | Routes for update cabin type
+    | Routes for assign user to cabin
+    */
+    /* Listing cabins */
+    Route::get('/cabins', 'CabinController@index'); //http://cabinapi.app/api/cabins?page=9
+
+    /* Listing other cabins */
+    Route::get('/cabins/othercabins', 'CabinController@indexOtherCabin'); //http://cabinapi.app/api/cabins/othercabins?page=9
+
+    /* Get information of cabin */
+    Route::get('/cabins/{id}', 'CabinController@show');
+
+    /* Get cabin price */
+    Route::get('/cabins/price/{id}', 'CabinController@showPrice');
+
+    /* Update cabin price */
+    Route::put('/cabins/price/{id}', 'CabinController@updatePrice');
+
+    /* Assign user to cabin */
+    Route::put('/cabins/assign/{userId}/{id}', 'CabinController@assignUser');
+
+    /* Update cabin type */
+    Route::put('/cabins/type/{type}/{id}', 'CabinController@updateType');
+
+    /* Delete cabin */
+    Route::delete('/cabins/{id}', 'CabinController@destroy');
+
+
+});
+
+
+/* Statistics purpose */
+Route::get('/cabins/name/{bookingCabinName}', 'CabinController@statistics');
