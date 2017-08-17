9ec076183f694fe6e728abec95c9946b0cc25146
sarath <sarath@cabin-holiday.com>
2017-08-17 12:34:48 +0200

    API-21: Removed extra column in export section

--- public/js/mountSchoolBookings.js
+++ public/js/mountSchoolBookings.js
@@ -83,19 +83,19 @@ $(function () {
                 {
                     extend: 'csv',
                     exportOptions: {
-                        columns: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+                        columns: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
                     }
                 },
                 {
                     extend: 'excel',
                     exportOptions: {
-                        columns: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+                        columns: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
                     }
                 },
                 {
                     extend: 'print',
                     exportOptions: {
-                        columns: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+                        columns: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
                     }
                 },
             ]
