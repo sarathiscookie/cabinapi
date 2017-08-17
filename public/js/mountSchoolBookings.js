3dc82085db3b060212f5ae83aafc83c55a0596be
sarath <sarath@cabin-holiday.com>
2017-08-17 11:32:20 +0200

    API-21: Clean up.

--- public/js/mountSchoolBookings.js
+++ public/js/mountSchoolBookings.js
@@ -17,7 +17,7 @@ $(function () {
 
     function fetch_data(is_date_search, daterange)
     {
-        booking_data = $('#booking_data').DataTable({
+        booking_data = $('#booking_data_mschool').DataTable({
                 "lengthMenu": [10, 50, 100, 250, 500],
                 "order": [[ 1, "desc" ]],
                 "processing": true,
