a603412bd76ec93b782d8efc2eadf33700cd718a
sarath <sarath@cabin-holiday.com>
2017-09-19 14:06:13 +0200

    API-29: Hided sidebar menu and count if inquiry list

--- resources/views/cabinowner/layouts/app.blade.php
+++ resources/views/cabinowner/layouts/app.blade.php
@@ -55,26 +55,19 @@
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
                     <!-- Messages: style can be found in dropdown.less-->
-                    <!-- <li class="dropdown messages-menu">
+                    {{--<li class="dropdown messages-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <i class="fa fa-envelope-o"></i>
                             <span class="label label-success">4</span>
                         </a>
-                    </li> -->
-                    <!-- Notifications: style can be found in dropdown.less -->
-                    <!-- <li class="dropdown notifications-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="fa fa-bell-o"></i>
-                            <span class="label label-warning">10</span>
-                        </a>
-                    </li> -->
+                    </li>--}}
                     <!-- Tasks: style can be found in dropdown.less -->
-                    <!-- <li class="dropdown tasks-menu">
+                    {{--<li class="dropdown tasks-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <i class="fa fa-flag-o"></i>
                             <span class="label label-danger">9</span>
                         </a>
-                    </li> -->
+                    </li>--}}
                     <!-- User Account: style can be found in dropdown.less -->
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -149,9 +142,9 @@
                         <li>
                             <a href="/cabinowner/bookings"><i class="fa fa-circle-o"></i> @lang('menu.bookingListMenu') <span class="pull-right-container"><span class="label label-primary pull-right">{!! $miscellaneous->bookingCount() !!}</span></span></a>
                         </li>
-                        <li>
+                        {{--<li>
                             <a href="/cabinowner/inquiry"><i class="fa fa-circle-o"></i> @lang('menu.inquiryList') <span class="pull-right-container"><span class="label label-primary pull-right">{!! $miscellaneous->inquiryListCount() !!}</span></span></a>
-                        </li>
+                        </li>--}}
                     </ul>
                 </li>
                 <li class="treeview">
@@ -163,9 +156,9 @@
                         <li>
                             <a href="/cabinowner/mschool/bookings"><i class="fa fa-circle-o"></i> @lang('menu.bookingListMschoolMenu') <span class="pull-right-container"><span class="label label-primary pull-right">{!! $miscellaneous->mSchoolBookingCount() !!}</span></span></a>
                         </li>
-                        <li>
+                        {{--<li>
                             <a href=""><i class="fa fa-circle-o"></i> Mschool Inquiry List <span class="pull-right-container"><span class="label label-primary pull-right">1</span></span></a>
-                        </li>
+                        </li>--}}
                     </ul>
                 </li>
 
