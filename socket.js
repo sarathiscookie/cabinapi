6844331dfe6e6b993fd08557bdd6229f0f044582
sarath <sarath@cabin-holiday.com>
2017-10-11 09:04:03 +0200

    API-29: Live notification updated for https

--- socket.js
+++ socket.js
@@ -1,6 +1,17 @@
+//connection must be made via SSL
+var https = require('https'),
+    fs =    require('fs');
+
+//SSL cert and key
+var options = {
+    cert:   fs.readFileSync('/etc/letsencrypt/live/dev02.huetten-holiday.de/fullchain.pem'),
+    key:    fs.readFileSync('/etc/letsencrypt/live/dev02.huetten-holiday.de/privkey.pem')
+};
+
+var server = https.createServer(options);
 var app   = require('express')();
 var http  = require('http').Server(app);
-var io    = require('socket.io')(http);
+var io    = require('socket.io')(server);
 var redis = require('redis');
 
 io.on('connection', function(socket){
