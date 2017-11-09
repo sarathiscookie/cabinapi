c90ef0a734e2d62a0953a8fe3bcf0f029cd9bd04
Divya Gopikrishna <divya@cabin-holiday.com>
2017-11-09 19:27:35 +0530

    API-36: Added  image functionality

--- public/js/image.js
+++ public/js/image.js
@@ -61,4 +61,86 @@ $('.img_button').on('click', function(e){
 
 
     });
-});
+
+        // create image functioans
+        $(".export").prop('disabled', true);
+
+        function neueBildBearbeitung() {
+
+            var dkrm = new Darkroom('#target', {
+                // Size options
+                minWidth: 100,
+                minHeight: 100,
+                maxWidth: 1980,
+                maxHeight: 720,
+                ratio: 1980 / 720,
+                backgroundColor: '#fff',
+                // Plugins options
+                plugins: {
+                    //save: false,
+                    crop: {
+                        quickCropKey: 67, //key "c"
+                        //minHeight: 50,
+                        //minWidth: 50,
+                        ratio: 1980 / 720
+                    },
+                    //Was nach dem Speichern passieren soll.
+                    save: {
+                        callback: function () {
+                            if (1 != 1) {
+
+                            } else {
+                                this.darkroom.selfDestroy(); // Turn off the bar and cleanup
+                                var imageData = dkrm.canvas.toDataURL({
+                                    format: 'jpeg',
+                                    quality: 0.8
+                                });
+                                //Timeout um dem Dateileser Zeit zu geben
+                                window.setTimeout(function () {
+                                    $("#hidden_base64Logo").val(imageData);
+                                    $(".export").prop('disabled', false);
+
+                                }, 1000);
+
+                            }
+                        }
+                    }
+                },
+
+                // Post initialize script
+                initialize: function () {
+                    var cropPlugin = this.plugins['crop'];
+                    cropPlugin.requireFocus();
+                }
+            });
+        }
+
+        document.getElementById('logoUpload').onchange = function (evt) {
+
+            if (typeof dkrm === 'undefined' || variable === null) {
+            } else {
+                dkrm.selfDestroy();
+            }
+
+            var tgt = evt.target || window.event.srcElement,
+                files = tgt.files;
+            // FileReader support
+            if (FileReader && files && files.length) {
+                var fr = new FileReader();
+                fr.onload = function () {
+                    document.getElementById('target').src = fr.result;
+                }
+                fr.readAsDataURL(files[0]);
+                //Timeout um dem Dateileser Zeit zu geben.
+                window.setTimeout(function () {
+                    neueBildBearbeitung();
+                }, 1000);
+
+            }
+
+        }
+        $(".export").click(function (e) {
+            document.imageupload.submit();
+        })
+
+    });
