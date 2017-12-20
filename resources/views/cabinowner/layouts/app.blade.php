da6d86b54d3e4359d3f075d5edf579bf6cbb6631
sarath <sarath@cabin-holiday.com>
2017-12-20 13:33:14 +0100

    API-29: Socket.io test in https server

--- resources/views/cabinowner/layouts/app.blade.php
+++ resources/views/cabinowner/layouts/app.blade.php
@@ -252,22 +252,22 @@
     <!-- Socket io -->
     <script src="{{ asset('plugins/socket/socket.io.min.js') }}"></script>
     <script>
-        /*var socket = io('{{env('APP_URL')}}:3000');*/
+        var socket = io('{{env('APP_URL')}}:3000');
         /* Realtime message notification */
-        /*socket.on('message', function(data){
+        socket.on('message', function(data){
            if(data){
-               /!* var res = $.parseJSON(data);*!/
+               /* var res = $.parseJSON(data);*/
                $('.messages-menu').empty();
                $('.messages-menu').html(data);
            }
-        });*/
+        });
         /* Realtime inquiry notification */
-        /*socket.on('inquiryCount', function(data){
+        socket.on('inquiryCount', function(data){
             if(data){
                 $('.tasks-menu').empty();
                 $('.tasks-menu').html(data);
             }
-        });*/
+        });
     </script>
 
 
