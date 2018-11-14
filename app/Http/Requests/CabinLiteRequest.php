d28d2b2a82b30a1e34e5540b1e57496a6a4da4ec
sarath <sarath@cabin-holiday.com>
2018-11-14 06:25:44 +0100

    API-38: Translations updated

--- app/Http/Requests/CabinLiteRequest.php
+++ app/Http/Requests/CabinLiteRequest.php
@@ -92,32 +92,32 @@ class CabinLiteRequest extends FormRequest
     public function attributes()
     {
         if (\Lang::locale() == 'de') {
-            return ['firstname' => 'Der Vornmae',
-                'lastname' => 'Der Nachname',
-                'mobile' => 'Die Handynummer',
-                'telephone' => 'Die Telefonnummer ',
-                'Zip' => 'Die Zip',
-                'city' => 'Die Stadt',
-                'usrCity' => 'Die Stadt',
-                'street' => 'Die Straße',
-                'usrAddress' => 'Die Straße',
-                'usrZip' => 'Die Zip',
-                'country' => 'Das Land',
-                'usrCountry' => 'Das Land',
-                'cabin_name' => 'Die Hütte',
-                'cabin_code' => 'Die Hüttenkürzel',
-                'height' => 'Die Höhe ',
-                'deposit' => 'Die Anzahlung',
-                'cabin_owner' => 'Der  Hüttenbesitzer',
-                'tours' => 'Touren',
-                'booking_type' => 'Hütte ungsarten',
-                'availability' => 'Erreichbar von',
-                'check_in' => 'Der Check-in ab',
-                'check_out' => 'Der Check-in bis',
-                'company' => 'Der Firmenname',
-                'usrCompany' => 'Der Firmenname',];
-        }else {
-            return [];
+            return [
+                'firstname'         => 'Der Vornmae',
+                'lastname'          => 'Der Nachname',
+                'mobile'            => 'Die Handynummer',
+                'telephone'         => 'Die Telefonnummer ',
+                'Zip'               => 'Die Zip',
+                'city'              => 'Die Stadt',
+                'usrCity'           => 'Die Stadt',
+                'street'            => 'Die Straße',
+                'usrAddress'        => 'Die Straße',
+                'usrZip'            => 'Die Zip',
+                'country'           => 'Das Land',
+                'usrCountry'        => 'Das Land',
+                'cabin_name'        => 'Die Hütte',
+                'cabin_code'        => 'Die Hüttenkürzel',
+                'height'            => 'Die Höhe ',
+                'deposit'           => 'Die Anzahlung',
+                'cabin_owner'       => 'Der Hüttenbesitzer',
+                'tours'             => 'Touren',
+                'booking_type'      => 'Hütte ungsarten',
+                'availability'      => 'Erreichbar von',
+                'check_in'          => 'Der Check-in ab',
+                'check_out'         => 'Der Check-in bis',
+                'company'           => 'Der Firmenname',
+                'usrCompany'        => 'Der Firmenname'
+            ];
         }
     }
 
@@ -131,24 +131,22 @@ class CabinLiteRequest extends FormRequest
 
         if (\Lang::locale() == 'de') {
             return [
-                'halfboard_price.required_if' => 'Das Preis ist erforderlich, wenn das Feld Halbpension markiert ist.',
-                'usrZip.required' => ' Die Postleizahl muss angegeben werden.',
-                'usrCity.required' => 'Die Stadt muss angegeben werden.',
-                'usrAddress.required' => 'Das Stadt muss angegeben werden.',
-                'usrCountry.required' => 'Das Land muss angegeben werden.',
-                 'deposit.numeric' => 'Bitte Zahl angeben'
-
-
+                'halfboard_price.required_if'   => 'Das Preis ist erforderlich, wenn das Feld Halbpension markiert ist.',
+                'usrZip.required'               => ' Die Postleizahl muss angegeben werden.',
+                'usrCity.required'              => 'Die Stadt muss angegeben werden.',
+                'usrAddress.required'           => 'Das Stadt muss angegeben werden.',
+                'usrCountry.required'           => 'Das Land muss angegeben werden.',
+                'deposit.numeric'               => 'Bitte Zahl angeben'
             ];
         }
         else{
-            return [    'halfboard_price.required_if' => 'The Price field is required when Half Board field is checked.',
-                'usrZip.required' => 'The Zip field is required.',
-                'usrCity.required' => 'The city field is required.',
-                'usrAddress.required' => 'ddd The street field is required.',
-                'usrCountry.required' => 'The country field is required.'
+            return [
+                'halfboard_price.required_if'   => 'The Price field is required when Half Board field is checked.',
+                'usrZip.required'               => 'The Zip field is required.',
+                'usrCity.required'              => 'The city field is required.',
+                'usrAddress.required'           => 'ddd The street field is required.',
+                'usrCountry.required'           => 'The country field is required.'
                 ];
-
         }
 
     }
